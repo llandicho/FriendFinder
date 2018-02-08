ce5d73b83eaeaaf1c41513d4722a60ee5f9b0e57
Louella Landicho <louellalandicho@Louellas-MacBook-Pro.local>
2018-02-07 17:35:18 -0800

    fixed folders

---
+++ app/data/friends.js
@@ -0,0 +1,60 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Lou',
+    profilePic: 'https://www.familychoiceawards.com/wp-content/uploads/2014/04/Wiggleless-Dog-Back-Brace-200-x-200.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },
+
+
+  {
+    name: 'Lou',
+    profilePic: 'http://cdn.akc.org/Ebook/11_SeniorDog.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },
+
+
+    {
+    name: 'Lou',
+    profilePic: 'http://www.animalfriendsrescue.org/available/thumb/dog_callie.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+
+
+];
+
+module.exports = friendList;
